c7e03d8ae4d2d925d03cc710759d2cf70beb8259
tenisistaa <krzychu_ten@o2.pl>
2018-01-07 14:36:43 +0100

    rectangle with side check

--- app/Rectangle.js
+++ app/Rectangle.js
@@ -1,10 +1,35 @@
 var Rectangle = function(a,b) {
-  this.a = a;
-  this.b = b;
+  this.setA(a);
+  this.setB(b);
 }
 
 Rectangle.prototype.getPerimeter = function() {
   return 2 * (this.a + this.b);
+}
+
+Rectangle.prototype.setA = function(value) {
+  var a = parseFloat(value);
+
+  if(a > 0) {
+    this.a = a;
+    return true;
+  }
+
+  this.a = null;
+  return false;
+
+}
+
+Rectangle.prototype.setB = function(value) {
+  var b = parseFloat(value);
+
+  if(b > 0){
+    this.b = b;
+    return true;
+  }
+
+  this.b = null;
+  return false;
 }
 
 module.exports = Rectangle;
--- spec/RectangleSpec.js
+++ spec/RectangleSpec.js
@@ -1,9 +1,34 @@
 var Rectangle = require('./../app/Rectangle.js');
 
-describe('Rectangle with sides 4 and 5', function() {
+describe('Rectangle', function() {
   var rectangle = new Rectangle(4, 5);
 
-  it('should return 18 when we calculated perimeter', function() {
+  describe('when set 0 for a', function() {
+    it('should return false', function() {
+      var result = rectangle.setA(0);
+
+      expect(result).toBeFalsy();
+    })
+
+    it('set null for prop a', function() {
+      expect(rectangle.a).toBeNull();
+    })
+
+  })
+
+  describe('when set 0 for b', function() {
+    it('should return false', function() {
+      result = rectangle.setB(0);
+
+      expect(result).toBeFalsy();
+    })
+
+    it('set null for prop b', function() {
+      expect(rectangle.b).toBeNull();
+    })
+  })
+
+  xit('should return 18 when we calculated perimeter', function() {
     expect(rectangle.getPerimeter()).toBe(18);
   })
 })
